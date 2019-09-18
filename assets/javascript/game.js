fc7206c748409aabe1ae8fc0c92a956a71a03eff
Perry Williams <perrywilliams@800appliance.com>
2019-09-17 20:31:19 -0500

    Can search by topic and it will retrieve 10 gifs (all moving images..)!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,4 @@
-      // Performing GET requests to the OMDB API and logging the responses to the console
-
-
-
-
-
-
-
-
-
+var response={};
 
 
 
@@ -17,17 +8,44 @@
 
 
 
-      function doThatThing (topic){
+      function getGiphyData (topic){
+
+
+            // Selecting the input element and get its value 
+            var newTopic = document.getElementById("topic").value;
+            
+            // Displaying the value
+// alert(newTopic);
+
       $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&q="+topic+"&offset=0&rating=R&lang=en&limit=5",
+        url: "https://api.giphy.com/v1/gifs/search?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&q="+newTopic+"&offset=0&rating=R&lang=en&limit=10",
         method: "GET"
       }).then(function(response) {
         console.log(response);
 
-for(var i=0;i<response.data.length;i++){
-        document.getElementById("results").innerHTML+="<img src='"+response.data[i].images.original.url+"'>";
-}
+        document.getElementById("results").innerHTML="";
+        for(var i=0;i<response.data.length;i++){
+                document.getElementById("results").innerHTML+="<img src='"+response.data[i].images.original.url+"'>";
+        }
+        
 
 
     });
+
+    
+
+
+  //  function refreshGifs(){
+    // save old stuff 
+        // OldGifs=document.getElementById("results").innerHTML;
+
+        // data[1].images.fixed_height_small
+        // .images.original.url
+
+// add old stuff to the end..
+// document.getElementById("results").innerHTML+=OldGifs;
     }
+    
+
+
+    // https://api.giphy.com/v1/gifs/random?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&tag=&rating=R&lang=en&limit=10
