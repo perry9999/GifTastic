8c43a99970da65597dd06d084b52707f12c3cdd4
Perry Williams <perrywilliams@800appliance.com>
2019-09-17 23:04:23 -0500

    displays 10 moving gifs of the topic of choice, has 10 buttons to stop motion..

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,44 @@
-var response={};
-
-
-
+var gifData = [];
 // https://api.giphy.com/v1/gifs/search?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&q=face palm&limit=5&offset=0&rating=R&lang=en
 
 
+function getGiphyData(topic) {
+    var newGifInnerHtml="";
 
+    // Selecting the input element and get its value 
+    var newTopic = document.getElementById("topic").value;
 
+    // Displaying the value
+    // alert(newTopic);
 
-      function getGiphyData (topic){
-
-
-            // Selecting the input element and get its value 
-            var newTopic = document.getElementById("topic").value;
-            
-            // Displaying the value
-// alert(newTopic);
-
-      $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&q="+newTopic+"&offset=0&rating=R&lang=en&limit=10",
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&q=" + newTopic + "&offset=0&rating=R&lang=en&limit=10",
         method: "GET"
-      }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
+       document.getElementById("results").innerHTML = "";
+        for (var i = 0; i < response.data.length; i++) {
+            newGifInnerHtml+= "<img id='gif-"+i+"'  src='" + response.data[i].images.fixed_height.url + "'>";
+            gifData.push(response.data[i]);
+            }
 
-        document.getElementById("results").innerHTML="";
-        for(var i=0;i<response.data.length;i++){
-                document.getElementById("results").innerHTML+="<img src='"+response.data[i].images.original.url+"'>";
-        }
-        
+            document.getElementById("results").innerHTML = newGifInnerHtml;
 
+            // document.getElementById("results").innerHTML += "<img src='" + response.data[i].images.fixed_height.url + "'>";
+    
 
     });
-
-    
-
-
-  //  function refreshGifs(){
-    // save old stuff 
-        // OldGifs=document.getElementById("results").innerHTML;
-
-        // data[1].images.fixed_height_small
-        // .images.original.url
-
-// add old stuff to the end..
-// document.getElementById("results").innerHTML+=OldGifs;
-    }
-    
+}
 
 
+function stopMotion(gifId){
+
+
+    document.getElementById("gif-"+gifId).src = gifData[gifId].images.fixed_height_still.url;
+
+}
+
+function logGifData(){
+console.log(gifData);
+}
     // https://api.giphy.com/v1/gifs/random?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&tag=&rating=R&lang=en&limit=10
