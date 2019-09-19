cccb9a2eb0f94deba3b946484f2c3c6fba393f33
Perry Williams <perrywilliams@800appliance.com>
2019-09-18 23:26:32 -0500

    Added START ALL and STOP ALL buttons, added comments to code, updated readme and deleted unnecessary files.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # GifTastic
 Homework 9/2019
+
+
+## General info
+
+Challenge was to create a interactive Gif display showing top 10 Gif's by topic, using Giphy API. Also required to start and stop gif's by clicking on them individually. This is accomplished by swapping out the img src attribute between the Giphy fixed height url and the Giphy fixed height still image.
+
+## Technologies
+* Html 5
+* CSS
+* Jquery 3.2.1
+
+
+## Status
+* Completed for homework 09-18-2019
+
+## Inspiration
+MTV, Aol, Compuserve, Commodore PET, Commodore 64, Android Phones
+
+## Contact
+Created by [https://www.linkedin.com/in/perry-williams-74240647], perrywilliams@800appliance.com - 1-800-4-PerryW. Feel free to contact me!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,31 @@
 var buttonChoices = ["Demolition Implosions", "Fail", "Bike Crash", "Baby Goats", "Elephants", "Fish"];
-var currentTopic="";
-
-
-function createButtons()
-    {
-    document.getElementById("buttonsDiv").innerHTML =""; 
-        for(var i=0;i<buttonChoices.length;i++){
-        varNewButton = "<button class='buttonToClick' onclick = 'getGiphyData(\""+buttonChoices[i]+"\")' >" +buttonChoices[i]+ "</button>";
+// creates ready-to-click buttons using topics in above array.. (buttonChoices)
+function createButtons() {
+    document.getElementById("buttonsDiv").innerHTML = "";
+    for (var i = 0; i < buttonChoices.length; i++) {
+        varNewButton = "<button class='buttonToClick' onclick = 'getGiphyData(\"" + buttonChoices[i] + "\")' >" + buttonChoices[i] + "</button>";
         document.getElementById("buttonsDiv").innerHTML += varNewButton;
-        }
     }
+    // Start All and Stop All buttons
+    varNewButton = "<button class='buttonToClick' onclick = 'startAll()' >Start ALL Gifs</button>";
+    document.getElementById("buttonsDiv").innerHTML += varNewButton;
+    varNewButton = "<button class='buttonToClick' onclick = 'stopAll()' >Stop ALL Gifs</button>";
+    document.getElementById("buttonsDiv").innerHTML += varNewButton;
+}
 
-
+// Add event listener to react when user enters new topic.. Creates button and automatically pull related updated Gif's.
 document.getElementById("addTopic").addEventListener("click", function () {
-    // buttonChoices.push( document.getElementById("topic").value);
-    buttonChoices.push( buttonChoices.push( document.getElementById("topic").value) );
+    buttonChoices.push(document.getElementById("topic").value);
     createButtons();
-    getGiphyData( document.getElementById("topic").value);
+    getGiphyData(document.getElementById("topic").value);
     console.log(document.getElementById("topic").value);
 });
+
 
 function getGiphyData(topic) {
     var newGifInnerHtml = "";
     document.getElementById("results").innerHTML = "Gif's coming Soon!";
-    currentTopic=topic;
-
-
-
     // Selecting the input element and get its value 
-    // var newTopic = document.getElementById("topic").value;
-
-    //   var newTopic = document.getElementById("topic").value;
-    //var newTopic=topic;
 
     var response = [];
     response.length = 0;
@@ -41,19 +35,20 @@ function getGiphyData(topic) {
     }).then(function (response) {
         console.log(response);
         document.getElementById("results").innerHTML = "";
+
+        // Start a for loop to create giph diplay div's with onclick events to start and stop motion.
         for (var i = 0; i < response.data.length; i++) {
             var newDiv = "";
-
-            newDiv =" <div class='gifBox'> <img class='gifDiv' topic='"+topic+"' id='gif-" + i + "' onclick='stopMotion(" + i + ")' now='moving' stillUrl='" + response.data[i].images.fixed_height_still.url + "' movingUrl='" + response.data[i].images.fixed_height.url + "' src='" + response.data[i].images.fixed_height.url + "'><div class='gifTitleDiv'>\""+topic+"\"  Rated: "+ response.data[i].rating +"</div></div>";
+            newDiv = " <div class='gifBox'> <img class='gifDiv' topic='" + topic + "' id='gif-" + i + "' onclick='stopStartMotion(" + i + ")' now='moving' stillUrl='" + response.data[i].images.fixed_height_still.url + "' movingUrl='" + response.data[i].images.fixed_height.url + "' src='" + response.data[i].images.fixed_height.url + "'><div class='gifTitleDiv'>\"" + topic + "\"  Rated: " + response.data[i].rating + "</div></div>";
             document.getElementById("results").innerHTML += newDiv;
         }
-    
+
     });
 }
 
 
 
-function stopMotion(gifId) {
+function stopStartMotion(gifId) {
     //If someone clicks on a gif..
     // get the value of the gif div that was clicked.
     theStillURL = document.getElementById("gif-" + gifId).getAttribute("stillUrl");
@@ -74,17 +69,27 @@ function stopMotion(gifId) {
 }
 
 
+// Start All / Stop All
+function startAll() {
+     for (var i = 0; i < 10; i++) {
+        // theStillURL = document.getElementById("gif-" + i).getAttribute("stillUrl");
+        var theMovingURL = document.getElementById("gif-" + i).getAttribute("movingUrl");
+
+        document.getElementById("gif-" + i).setAttribute("src", theMovingURL);
+        document.getElementById("gif-" + i).setAttribute("now", "moving");
+    }
+}
+function stopAll() {
+    for (var i = 0; i < 10; i++) {
+        theStillURL = document.getElementById("gif-" + i).getAttribute("stillUrl");
+        theMovingURL = document.getElementById("gif-" + i).getAttribute("movingUrl");
+        document.getElementById("gif-" + i).setAttribute("src", theStillURL);
+        document.getElementById("gif-" + i).setAttribute("now", "still");
+    }
+}
+
+
+
 // create default buttons
 createButtons();
-  
-
-
-
-
-
-
-
-
-
-
 
