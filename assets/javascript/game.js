fec9b708cb201c12a69963696a4428f09412773d
Perry Williams <perrywilliams@800appliance.com>
2019-09-18 19:13:13 -0500

    Working site.. all gifs load moving, but toggle still/moving on click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,19 @@
-var gifData = [];
-// https://api.giphy.com/v1/gifs/search?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&q=face palm&limit=5&offset=0&rating=R&lang=en
+
+
+document.getElementById("findGiphys").addEventListener("click",function(){
+    var newTopic = document.getElementById("topic").value;
+    getGiphyData(newTopic);  //run getGiphyData function..
+});
 
 
 function getGiphyData(topic) {
     var newGifInnerHtml="";
+    document.getElementById("results").innerHTML="Gif's coming Soon!";
 
     // Selecting the input element and get its value 
     var newTopic = document.getElementById("topic").value;
-
-    // Displaying the value
-    // alert(newTopic);
-
+    var response=[];
+    response.length=0;
     $.ajax({
         url: "https://api.giphy.com/v1/gifs/search?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&q=" + newTopic + "&offset=0&rating=R&lang=en&limit=10",
         method: "GET"
@@ -18,27 +21,54 @@ function getGiphyData(topic) {
         console.log(response);
        document.getElementById("results").innerHTML = "";
         for (var i = 0; i < response.data.length; i++) {
-            newGifInnerHtml+= "<img id='gif-"+i+"'  src='" + response.data[i].images.fixed_height.url + "'>";
-            gifData.push(response.data[i]);
-            }
+            // onclick='stopMotion("+i+")'
+            document.getElementById("results").innerHTML+="<img class='gifDiv' id='gif-"+i+"'  now='moving' stillUrl='"+response.data[i].images.fixed_height_still.url+"' movingUrl='"+response.data[i].images.fixed_height.url+"' src='" + response.data[i].images.fixed_height.url + "'>";
 
-            document.getElementById("results").innerHTML = newGifInnerHtml;
+            document.getElementById("gif-"+i).addEventListener("click",function(){ stopMotion("gif-"+i);    });
+            
 
+
+
+        }
+
+
+
+            // document.getElementById("results").innerHTML = newGifInnerHtml;
+
+                        //create listener
+ 
+            
             // document.getElementById("results").innerHTML += "<img src='" + response.data[i].images.fixed_height.url + "'>";
-    
 
     });
 }
 
+// element.addEventListener("mouseover", myFunction);
+// element.addEventListener("click", mySecondFunction);
+// element.addEventListener("mouseout", myThirdFunction);
 
-function stopMotion(gifId){
 
+ function stopMotion(gifId){
+    //If someone clicks on a gif..
+// get the value of the gif div that was clicked.
 
-    document.getElementById("gif-"+gifId).src = gifData[gifId].images.fixed_height_still.url;
+// console.log("Before if then, now  = "+document.getElementById("gif-"+gifId).getAttribute("now"))
+   
 
-}
+    theStillURL=    document.getElementById("gif-"+gifId).getAttribute("stillUrl");
+    theMovingURL=    document.getElementById("gif-"+gifId).getAttribute("movingUrl");
+    theNow =     document.getElementById("gif-"+gifId).getAttribute("now"); 
+    // console.log("Before if then, theNow  = "+theNow)
+ 
+            if(theNow==="moving"){
+                // console.log("if moving -theNow = "+theNow);
+                document.getElementById("gif-"+gifId).setAttribute("src",theStillURL); 
+                document.getElementById("gif-"+gifId).setAttribute("now","still"); 
+            }else{
+                // console.log("else -theNow = "+theNow);
+                document.getElementById("gif-"+gifId).setAttribute("src",theMovingURL); 
+                document.getElementById("gif-"+gifId).setAttribute("now","moving"); 
+            }
+            // console.log("AFter if then, now  = "+document.getElementById("gif-"+gifId).getAttribute("now"))
+        }
 
-function logGifData(){
-console.log(gifData);
-}
-    // https://api.giphy.com/v1/gifs/random?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&tag=&rating=R&lang=en&limit=10
